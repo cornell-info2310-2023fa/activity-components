9a56a7d1e83f35d4ab3060178d285a72757cf79f
Kyle J. Harms <kyle.harms@cornell.edu>
2023-08-29 09:10:12 -0400

    Header and DateRefresh components

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Header from './Header';
+import DateRefresh from './DateRefresh';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    TODO
+    <Header />
+    <DateRefresh />
   </React.StrictMode>
 );
